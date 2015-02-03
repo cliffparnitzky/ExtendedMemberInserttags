fea2c84f20846ae103078ac6cf3904a513cbbf8a
Cliff Parnitzky <github@cliff-parnitzky.de>
2015-02-03 17:57:14 +0100

    Refactoring to only Contao 3 compatibility

--- system/modules/ExtendedMemberInserttags/classes/ExtendedMemberInserttags.php
+++ system/modules/ExtendedMemberInserttags/classes/ExtendedMemberInserttags.php
@@ -1,8 +1,8 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2014 Leo Feyer
+ * Copyright (C) 2005-2015 Leo Feyer
  *
  * Formerly known as TYPOlight Open Source CMS.
  *
@@ -21,21 +21,26 @@
  * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  Cliff Parnitzky 2012-2014
+ * @copyright  Cliff Parnitzky 2012-2015
  * @author     Cliff Parnitzky
  * @package    ExtendedMemberInserttags
  * @license    LGPL
  */
 
 /**
+ * Run in a custom namespace, so the class can be replaced
+ */
+namespace ExtendedMemberInserttags;
+
+/**
  * Class ExtendedMemberInserttags
  *
  * InsertTag hook class.
- * @copyright  Cliff Parnitzky 2012-2014
+ * @copyright  Cliff Parnitzky 2012-2015
  * @author     Cliff Parnitzky
  * @package    Controller
  */
-class ExtendedMemberInserttags extends Controller
+class ExtendedMemberInserttags extends \Controller
 {
 	public function __construct()
 	{
@@ -44,7 +49,7 @@ class ExtendedMemberInserttags extends Controller
 	}
 	
 	/**
-	 * Replaces the additional member inserttags 
+	 * Replaces the additional member inserttags
 	 */
 	public function replaceExtendedMemberInserttags($strTag)
 	{
--- system/modules/ExtendedMemberInserttags/config/autoload.php
+++ system/modules/ExtendedMemberInserttags/config/autoload.php
@@ -3,7 +3,7 @@
 /**
  * Contao Open Source CMS
  *
- * Copyright (c) 2005-2014 Leo Feyer
+ * Copyright (c) 2005-2015 Leo Feyer
  *
  * @package ExtendedMemberInserttags
  * @link    https://contao.org
@@ -12,9 +12,19 @@
 
 
 /**
+ * Register the namespaces
+ */
+ClassLoader::addNamespaces(array
+(
+	'ExtendedMemberInserttags',
+));
+
+
+/**
  * Register the classes
  */
 ClassLoader::addClasses(array
 (
-	'ExtendedMemberInserttags' => 'system/modules/ExtendedMemberInserttags/ExtendedMemberInserttags.php',
+	// Classes
+	'ExtendedMemberInserttags\ExtendedMemberInserttags' => 'system/modules/ExtendedMemberInserttags/classes/ExtendedMemberInserttags.php',
 ));
